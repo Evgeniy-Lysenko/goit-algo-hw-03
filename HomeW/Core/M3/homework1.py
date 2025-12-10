58a7b8ccb41f425c115b54027b77cdfd448d54e7
Евгений Лысенко <udjiedwards@gmail.com>
2025-12-11 00:39:17 +0200

    checking  input  date

--- HomeW/Core/M3/homework1.py
+++ HomeW/Core/M3/homework1.py
@@ -1,4 +1,17 @@
 # the number of days between a given date and a current date
-from datetime import datetime, date
-today_date = datetime.now() # current date and time
-print(today_date.date()) # 
+from datetime import datetime
+today_date = datetime.now() # Отримання поточнї дату
+def get_valid_date(): # Перевірка введенної дати
+    while True:
+        input_date = input("Enter a date in the format YYYY-MM-DD: ")
+        try:
+            return datetime.strptime(input_date, "%Y-%m-%d")
+        except ValueError:
+            print("Invalid date format. Please try again.")
+
+input_date = get_valid_date()
+
+
+days = (today_date - input_date).days  
+print(days)
+
