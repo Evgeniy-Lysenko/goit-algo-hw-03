a6d7003dc3429ddeddba2d63eb6066aadb8d827b
Евгений Лысенко <udjiedwards@gmail.com>
2025-12-17 00:00:07 +0200

    add comments

--- homework1.py
+++ homework1.py
@@ -5,14 +5,14 @@ def get_valid_date(): # Перевірка введенної дати
     while True:
         input_date = input("Enter a date in the format YYYY-MM-DD: ")
         try:
-            return datetime.strptime(input_date, "%Y-%m-%d")
+            return datetime.strptime(input_date, "%Y-%m-%d") # Перевірка формату дати
         except ValueError:
-            print("Invalid date format. Please try again.")
+            print("Invalid date format. Please try again.") # Повідомлення про помилку при неправильному форматі дати
 
 input_date = get_valid_date()
 
 def get_days_from_today(date): # Розрахунок кількості днів між двома датами
     return (today_date - date).days
  
-print(get_days_from_today(input_date))
+print(get_days_from_today(input_date)) # Вивід кількості днів між двома датами
 
